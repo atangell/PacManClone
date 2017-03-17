24e3d6efcbe2c0ec65b8279b065376a9e2f371bd
AMARENDER REDDY <AMARENDER REDDY>
2017-03-17 13:33:53 -0400

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Transform myTransform;
+    Vector3 direction;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myTransform = this.gameObject.GetComponent<Transform>();
+        direction = new Vector3(1, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            //move pacman up
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+            direction = new Vector3(0, 1, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            //move pacman up
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(270, Vector3.forward);
+            direction = new Vector3(0, -1, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            //move pacman up
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            direction = new Vector3(-1, 0, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            //move pacman up
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            direction = new Vector3(1, 0, 0);
+
+        }
+
+
+
+        //   Vector3 move = new Vector3(h, v, 0);
+
+        myTransform.position += direction * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public float speed = 0.4f;
+    Vector2 vect = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+
+        vect = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector2 p = Vector2.MoveTowards(transform.position, vect, speed);
+        GetComponent < Rigidbody2D>().MovePosition(p);
+
+        if ((Vector2)transform.position == vect)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                vect = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) )
+                vect = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) )
+                vect = (Vector2)transform.position - Vector2.down;
+            if (Input.GetKey(KeyCode.LeftArrow) )
+                vect = (Vector2)transform.position - Vector2.left;
+        }
+    }
+
+    //bool valid (Vector2 dir)
+    //{
+    //    Vector2 pos = transform.position;
+    //    RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+    //    return (hit.collider == GetComponent<Collider2D>());
+    //}
+}
